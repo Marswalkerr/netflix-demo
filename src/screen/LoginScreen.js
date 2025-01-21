ae72de551bd98144baacc9f17d266181d641aa59
Sanket Savani <sanketsavani004@gmail.com>
2025-01-21 12:41:49 +0530

    fixed logo rendering

--- src/screen/LoginScreen.js
+++ src/screen/LoginScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./LoginScreen.css";
 import SignUpScreen from "./SignUpScreen";
+import netflixLogo from "../images/netflixLogo.png"
 
 export default function LoginScreen() {
     const [signIn, setSignIn] = React.useState(false);
@@ -10,7 +11,7 @@ export default function LoginScreen() {
             <div className="loginScrreenBg">
                 <img className="loginScreenLogo"
                     alt="netflixLogo"
-                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" />
+                    src={netflixLogo} />
                 <button onClick={() => setSignIn(true)} className="loginScreenButton">Sign In</button>
                 <div className="loginScreenGradient"></div>
             </div>
